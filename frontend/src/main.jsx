d692e5fa4e1b5580ff7029e1e1c58f62bf484554
haibemed20-droid <Haibemed20@gmail.com>
2026-08-05 00:17:48 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,9 @@ import './index.css'
 import './styles/vendorShopProfile.css'
 import './styles/vendorProductListing.css'
 import './styles/adminApproval.css'
+import './styles/adminProductManagement.css'
+import './styles/adminVendorManagement.css'
+import './styles/shopCatalog.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
