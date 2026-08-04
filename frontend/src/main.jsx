e01d18d66481f4ebc0a6a6831344b45cbcb17337
haibemed20-droid <Haibemed20@gmail.com>
2026-08-04 23:59:22 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './styles/vendorShopProfile.css'
 import './styles/vendorProductListing.css'
+import './styles/adminApproval.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
